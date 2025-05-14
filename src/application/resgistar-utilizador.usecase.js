8b99ab9dc6c16a42499b46d8487b3c768d3da1cc
ProgramandoComAndre <andrerafael112@gmail.com>
2025-05-14 12:23:26 +0100

    criar registo de user

---
+++ src/application/resgistar-utilizador.usecase.js
@@ -0,0 +1,11 @@
+module.exports = function registarUtilizadorUseCase(userRepository) {
+    return async function ({nomeCompleto, NIF, telefone, morada, email}) {
+        await userRepository.register({
+            nomeCompleto,
+            NIF,
+            telefone,
+            morada,
+            email
+        })
+    }
+}
